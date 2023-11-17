906be2b124017fba3dcb8d6575dd1ea036a5884e
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2023-11-17 23:45:19 +0100

    fixv2: output export config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
- 
-    // Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    skipTrailingSlashRedirect: true,
- 
-    // Change the output directory `out` -> `dist`
-    distDir: 'dist',
+    output: 'export'
 }
 
 module.exports = nextConfig
