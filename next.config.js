43e6200a761b3f437dda3f859bdf18b1e46e2778
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2023-11-18 01:20:51 +0100

    next.js config & workflow bumps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    skipTrailingSlashRedirect: true,
+    distDir: 'dist',
 }
 
 module.exports = nextConfig
