0d8c3734194434f2cf44b93d9ff3f966c48a4330
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2024-01-21 20:02:48 +0100

    chore(scope): update year in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-auto">
       <div className="mx-auto w-full max-w-screen-xl p-6 md:py-8">
         <span className="text-center block text-sm text-muted-foreground">
-          © {new Date().getFullYear()}{" "}
+          © 2023-{new Date().getFullYear()} by{" "}
           <a
             target="_blank"
             href="https://github.com/kshyk"
