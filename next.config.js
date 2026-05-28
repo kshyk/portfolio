28c242f58e395506986f9daf31b26e709d0ddd9a
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2026-05-29 00:17:52 +0200

    Revert "Feat/autonoma (#1141)" (#1142)
    This reverts commit 9624cdd89675cd21e1a76d4e9fdd196da0a67619.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    ...(process.env.NODE_ENV === 'production' ? { output: 'export' } : {}),
+    output: 'export',
     skipTrailingSlashRedirect: true,
     distDir: 'dist',
 }
